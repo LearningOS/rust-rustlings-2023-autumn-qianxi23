46f4bec556071a80e487165685121b911f428a09
qianxi23 <116621273+qianxi23@users.noreply.github.com>
2023-10-21 22:10:24 +0800

    1

--- exercises/tests/tests7.rs
+++ exercises/tests/tests7.rs
@@ -36,7 +36,13 @@
 
 // I AM NOT DONE
 
-fn main() {}
+fn main() {
+    let timestamp = std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH).unwrap().as_secs();
+
+    // std::env::set_var("TEST_FOO", timestamp.to_string());
+    println!("cargo:rustc-env=TEST_FOO={}", timestamp.to_string());
+    println!("cargo:rustc-cfg=feature=\"pass\"");
+}
 
 #[cfg(test)]
 mod tests {
@@ -53,3 +59,4 @@ mod tests {
         assert!(timestamp >= e && timestamp < e + 10);
     }
 }
+
