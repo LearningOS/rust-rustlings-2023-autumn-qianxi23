53da010786bc38a48dfc5ebc446541206aeeee1f
qianxi23 <116621273+qianxi23@users.noreply.github.com>
2023-10-21 23:00:48 +0800

    1

--- exercises/tests/tests7.rs
+++ exercises/tests/tests7.rs
@@ -44,19 +44,3 @@ fn main() {
     println!("cargo:rustc-cfg=feature=\"pass\"");
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_success() {
-        let timestamp = std::time::SystemTime::now()
-            .duration_since(std::time::UNIX_EPOCH)
-            .unwrap()
-            .as_secs();
-        let s = std::env::var("TEST_FOO").unwrap();
-        let e: u64 = s.parse().unwrap();
-        assert!(timestamp >= e && timestamp < e + 10);
-    }
-}
-
