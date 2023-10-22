300f4ac2bf6729ddcc6db6e215886bd1aea2f175
qianxi23 <116621273+qianxi23@users.noreply.github.com>
2023-10-22 14:00:03 +0800

    1

--- exercises/tests/tests7.rs
+++ exercises/tests/tests7.rs
@@ -34,13 +34,24 @@
 // Execute `rustlings hint tests7` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
-fn main() {
-    let timestamp = std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH).unwrap().as_secs();
 
-    // std::env::set_var("TEST_FOO", timestamp.to_string());
-    println!("cargo:rustc-env=TEST_FOO={}", timestamp.to_string());
-    println!("cargo:rustc-cfg=feature=\"pass\"");
+fn main() { 
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_success() {
+        let timestamp = std::time::SystemTime::now()
+            .duration_since(std::time::UNIX_EPOCH)
+            .unwrap()
+            .as_secs();
+        let s = std::env::var("TEST_FOO").unwrap();
+        let e: u64 = s.parse().unwrap();
+        assert!(timestamp >= e && timestamp < e + 10);
+    }
 }
 
--- exercises/tests/tests8.rs
+++ exercises/tests/tests8.rs
@@ -7,7 +7,7 @@
 // Execute `rustlings hint tests8` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 fn main() {
     let timestamp = std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH).unwrap().as_secs();
